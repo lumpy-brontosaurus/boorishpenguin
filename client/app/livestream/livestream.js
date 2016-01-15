f9a6b41554e86120df06c49635b67945521e866b
Ronll <lapushnerron@gmail.com>
2016-01-15 11:12:20 -0800

    fixed livestream view to show the correct information

--- client/app/livestream/livestream.js
+++ client/app/livestream/livestream.js
@@ -5,7 +5,7 @@ livestream.controller('livestreamController', function($scope, $location,$routeP
     $scope.questions = [];
     Questions.getSession('/sessions/' + $routeParams.eventId, function (res) {
       //ask hridya what is this field, title? subject?
-      $scope.title = res.data.results[0].course;
+      $scope.subject = res.data.results[0].course;
       $scope.eventDate = res.data.results[0].time;
       $scope.author = res.data.results[0].user;
       console.log($scope.user)
