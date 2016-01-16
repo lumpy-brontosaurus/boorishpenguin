3cc87cab2d3ac0af1b954b2a903d068bdc16e079
hridhya <menonhridhya@gmail.com>
2016-01-16 12:49:27 -0800

    half working img upload

--- client/app/answers/answers.js
+++ client/app/answers/answers.js
@@ -1,6 +1,6 @@
-angular.module('boorish.answers', [])
+angular.module('boorish.answers', ["firebase"])
 
-.controller('answersController', function($scope, $location, $window, Answers, Questions, Users, Auth) {
+.controller('answersController', function($scope, $location, $window, Answers, Questions, Users, Auth, $firebaseObject) {
   $scope.data = {};
   $scope.newAnswer = {};
 
@@ -34,6 +34,43 @@ angular.module('boorish.answers', [])
       $scope.data.question = res.data.results[0];
       $scope.data.answers = res.data.results.slice(1);
     });
+  };
+
+  $scope.upload = function(){
+    Users.getUserWithId().then(function(userID) {
+
+      var refImg = new Firebase("https://joksina.firebaseio.com/images/" + userID);
+      var ImgObj = $firebaseObject(refImg);
+      
+      //function saveimage(e1) {
+      //  var filename = e1.target.files[0];
+      //  var fr = new FileReader();
+      //  fr.onload = function (res) {
+      //    $scope.image = res.target.result;
+      //    ImgObj.image = res.target.result;
+      //    ImgObj.$save().then(function (val) {
+      //    }, function (error) {
+      //      console.log("ERROR", error);
+      //    })
+      //  };
+      //  fr.readAsDataURL(filename);
+      //}
+      //
+      //document.getElementById("file-upload").addEventListener('change', saveimage, false);
+      //
+      //this.loadimage = function () {
+      //  ImgObj.$loaded().then(function (obj) {
+      //    console.log(obj.image);
+      //    $scope.profileImage = obj.image;
+      //    console.log("loaded", $scope.profileImage);
+      //    document.getElementById("profileImage").src = obj.image;
+      //  }, function (error) {
+      //    console.log("ERROR", error);
+      //  });
+      //};
+      //this.loadimage();
+    })
+
   };
 
   $scope.addAnswer = function() {
